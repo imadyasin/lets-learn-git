713acf6048adf481bb1ccaf962d4dc896f8ce849
Imad Yasin <imadyasin07@gmail.com>
2023-02-08 14:49:38 +0530

    addition-of-three

---
+++ src/AddtionOfthree.java
@@ -0,0 +1,9 @@
+public class AddtionOfthree {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int sum = a + b + c;
+        System.out.println(sum);
+    }
+}
