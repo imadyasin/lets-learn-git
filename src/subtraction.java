9f965586f9ad155f999da0bb08303805e3e80761
Imad Yasin <imadyasin07@gmail.com>
2023-02-08 14:12:08 +0530

    subtraction

---
+++ src/subtraction.java
@@ -0,0 +1,8 @@
+public class subtraction {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        int sub = a-b;
+        System.out.println(sub);
+    }
+}
